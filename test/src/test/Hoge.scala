aaa423b9113d24b673a9160795687d68fff3e777
taku <taku@tukemono>
2015-06-14 11:51:25 +0900

    branch test 1

---
+++ test/src/test/Hoge.scala
@@ -0,0 +1,9 @@
+package test
+
+class Hoge {
+  
+  def print(){
+    println("hoge!")
+  }
+
+}
--- test/src/test/Testmain.scala
+++ test/src/test/Testmain.scala
@@ -3,4 +3,5 @@ package test
 object Testmain extends App{
   println("hellow")
 
+  new Hoge().print
 }
